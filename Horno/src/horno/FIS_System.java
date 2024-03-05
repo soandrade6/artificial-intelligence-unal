ddc7cd385eaf1c8759b4c37074a14431d6f6c925
soandrade6 <sofiapl2018@gmail.com>
2024-03-05 14:55:55 -0500

    Sistema de inferencia difusa de un horno en Java

---
+++ Horno/src/horno/FIS_System.java
@@ -0,0 +1,54 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package horno;
+
+/**
+ *
+ * @author sofia
+ */
+
+import net.sourceforge.jFuzzyLogic.FIS;
+import net.sourceforge.jFuzzyLogic.plot.JFuzzyChart;
+public class FIS_System {
+    final private String path_Fis = "src/horno/FIS_horno";
+    final private FIS fis_var;
+    
+    public FIS_System() {
+        this.fis_var = FIS.load(this.path_Fis, true);
+        if (this.fis_var == null) {
+            System.err.println("Error al cargar el FIS");
+        }
+    }
+    
+    public String evaluar(double humedad, double intensidad, double volumen){
+        this.fis_var.setVariable("humedad", humedad);
+        this.fis_var.setVariable("intensidad", intensidad);
+        this.fis_var.setVariable("volumen" ,volumen);
+        this.fis_var.evaluate();
+        JFuzzyChart.get().chart(this.fis_var.getFunctionBlock("horno"));
+        return "Temperatura " +
+                this.fis_var.getVariable("temperatura").getLatestDefuzzifiedValue();        
+    }
+    
+    
+    public String get_CD(){
+
+        String[] CD_temperatura = {"baja", "media", "alta"};
+        String result = "";
+
+        for (String v1:CD_temperatura){
+            if (this.fis_var.getVariable("temperatura").getMembership(v1) > 0){
+                result += v1 + ", ";
+            }
+        }
+
+        result = result.strip();
+        StringBuilder sb = new StringBuilder(result);
+        sb.deleteCharAt(result.length()-1);
+
+        return sb.toString();
+    }
+    
+}
---
+++ Horno/src/horno/Main.java
@@ -0,0 +1,37 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package horno;
+
+import java.util.Scanner;
+
+/**
+ *
+ * @author sofia
+ */
+public class Main {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+        FIS_System fis = new FIS_System();
+
+        System.out.println("Ingrese la humedad en una escala de 0 a 10");
+        double humedad = scan.nextDouble();
+
+        System.out.println("Ingrese la intensidad en una escala de 0 a 10");
+        double intensidad = scan.nextDouble();
+
+        System.out.println("Ingrese el volumen en una escala de 0 a 10");
+        double volumen = scan.nextDouble();
+
+        String result = fis.evaluar(humedad, intensidad, volumen);
+
+        System.out.println(result);
+        System.out.println(fis.get_CD());
+    }
+    
+}
